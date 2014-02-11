3a9afc2f6c6720878dbe824dac8cb503aabf3bef
ig_2014 <ig_2014@igtp4-mac10.local>
2014-02-11 10:52:19 +0100

    Initial Commit

---
+++ appliSalon/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  appliSalon
+//
+//  Created by ig_2014 on 11/02/2014.
+//  Copyright (c) 2014 ig_2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ appliSalon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  appliSalon
+//
+//  Created by ig_2014 on 11/02/2014.
+//  Copyright (c) 2014 ig_2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
